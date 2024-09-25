1ece80daf03e1875717789a8c6620bf5a8627bf0
Ronald Pasion <52368740+RonaldPasion@users.noreply.github.com>
2024-09-25 14:44:51 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const myAge = 69;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+const myAgeInDogYears = earlyYears + laterYears;
+const myName = 'George'.toLowerCase();
+console.log(`My name is ${myName}, I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
